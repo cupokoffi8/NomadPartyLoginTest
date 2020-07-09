34d293e21db6b4983496628eecffc0f8e05949a6
Alex <cupokoffi@gmail.com>
2020-07-08 21:50:36 -0400

    Initial Commit

---
+++ NomadParty Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NomadParty Login
+//
+//  Created by Alex Gaskins on 7/8/20.
+//  Copyright © 2020 Alex Gaskins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
